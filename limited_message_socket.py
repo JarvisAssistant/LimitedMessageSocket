fa67e5121872f14e75c9fdcf23b8992bf0381ab4
EC2 Default User <ec2-user@ip-172-31-35-115.us-east-2.compute.internal>
2019-08-11 23:07:01 +0000

    Return send call

--- limited_message_socket.py
+++ limited_message_socket.py
@@ -24,29 +24,8 @@ class LimitedMessageSocket:
 		self.socket.close()
 	
 	def send(self, message):
-		self.socket.send(message.encode())
-		'''
-		totalsent = 0
-		while totalsent < LimitedMessageSocket.MSG_LEN:
-			sent = self.socket.send(msg[totalsent:])
-			if not sent:
-				raise RuntimeError('socket connection broken')
-			totalsent += sent
-		'''
+		return self.socket.send(message.encode())
 	
 	def receive(self):
 		return self.socket.recv(LimitedMessageSocket.MSG_LEN).decode()
-		'''
-		chunks = []
-		bytes_recd = 0
-		while bytes_recd < LimitedMessageSocket.MSG_LEN:
-			chunk = self.socket.recv(
-					min(LimitedMessageSocket.MSG_LEN - bytes_recd, 2048))
-			if chunk == b'':
-				raise RuntimeError('socket connection broken')
-			chunks.append(chunk)
-			bytes_recd += len(chunk)
-
-		return b''.join(chunks)
-		'''
 
